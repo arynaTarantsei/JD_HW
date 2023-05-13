06972d2372e9ac1840386810be6fa5215dc06795
“Aryna <arina.tarantey1998@gmail.com>
2023-05-13 23:18:55 +0300

    initial commit

---
+++ src/com/Homework5/MyClass.java
@@ -0,0 +1,26 @@
+package com.Homework5;
+
+public class MyClass {
+    private int myInt;
+    private char myChar;
+
+    public MyClass() {
+    }
+
+    public int getMyInt() {
+        return myInt;
+    }
+
+    public char getMyChar() {
+        return myChar;
+    }
+
+
+    public void setMyInt(int myInt) {
+        this.myInt = myInt;
+    }
+
+    public void setMyChar(char myChar) {
+        this.myChar = myChar;
+    }
+}
---
+++ src/com/Homework5/Task1.java
@@ -0,0 +1,9 @@
+package com.Homework5;
+
+public class Task1 {
+    public static void main(String[] args) {
+        MyClass myClass = new MyClass();
+        System.out.println("Поле int: "+myClass.getMyInt());
+        System.out.println("Поле char: "+myClass.getMyChar());
+    }
+}
