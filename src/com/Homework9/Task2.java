f6f9caa484c86a0cc37c55e2de870f52e358817f
“Aryna <arina.tarantey1998@gmail.com>
2023-05-17 00:26:19 +0300

    initial commit

--- src/com/Homework9/Task2.java
+++ src/com/Homework9/Task2.java
@@ -11,15 +11,17 @@ public class Task2 {
         System.out.println("Первый элемент второй пары: " + pair2.first());
         System.out.println("Второй элемент второй пары: " + pair2.last());
 
+        System.out.println();
         System.out.println("Обмен значениями между элементами первой пары: ");
         pair1.swap();
-        System.out.println(pair1.first());
-        System.out.println(pair1.last());
+        System.out.println("Первый элемент: " + pair1.first());
+        System.out.println("Второй элемент: " + pair1.last());
 
+        System.out.println(" ");
         System.out.println("Замена значений элементов второй пары новыми значениями: ");
         pair2.replaceFirst("How are you?");
         pair2.replaceLast(271);
-        System.out.println(pair2.first());
-        System.out.println(pair2.last());
+        System.out.println("Первый элемент: " + pair2.first());
+        System.out.println("Второй элемент: " + pair2.last());
     }
 }
